c04fb31afb709268455206c71731fa0bd7f3a03d
Jason Sun <js8965441@gmail.com>
2025-05-23 15:22:19 -0500

    milestone-6

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -8,6 +8,26 @@ first_play=true
   plant_leaves=2
   weather_conditions=(Rainy Sunny Cloudy Overcast Windstorm Rainy Foggy)
 
+#functions
+prompt_yes_no(){
+  local prompt=$1
+  while true do;
+  read -p "$prompt (yes/no): " answer
+  answer="$answer"
+  if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+    return 0
+  elif [[ "$answer" == "no" || "$answer" == "n" ]]; then
+    return 1
+  else
+    echo "invalid input please type yes or no"
+  fi done
+}
+
+get_weather(){
+  weather+${weather_conditions[$RANDOM % ${#weather_conditions[@]}]}
+}
+
+
 echo "Hello, I'm Tom. Welcome to ULTRA SIGMA MAKER 2000 V2 PLANTGROWER!!!"
 read -p "What is your name? " name
 echo "Hello $name. I have entrusted my garden to you."
